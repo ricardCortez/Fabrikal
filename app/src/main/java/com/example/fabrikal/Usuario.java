46cab58c41c69286d18851c0d67bd7f26e169edd
Dulin <u201711244@upc.edu.pe>
2021-04-25 15:43:57 -0500

    Actualizacion_V3

---
+++ app/src/main/java/com/example/fabrikal/Usuario.java
@@ -0,0 +1,135 @@
+package com.example.fabrikal;
+
+public class Usuario {
+
+    private  String uid;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String tipo_direccion;
+    private String direccion;
+    private String nro_lote;
+    private String provincia ;
+    private String distrito;
+    private String celular;
+    private String email ;
+    private String contraseña;
+    private String nro_tarjeta;
+    private  String vigencia_tarjeta;
+
+    public String getuid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+
+    }
+
+    public String getsdni() {
+        return dni;
+    }
+
+    public void setDni(String dni ) {
+       dni = dni;
+    }
+
+    public String getsnombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre ) {
+        nombre = nombre;
+    }
+
+
+    public String getsapellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido ) {
+        apellido = apellido;
+    }
+
+    public String getstipo_direccion() {
+        return tipo_direccion;
+    }
+
+    public void setTipo_direccion(String tipo_direccion ) {
+        tipo_direccion = tipo_direccion;
+    }
+
+    public String getsdireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion ) {
+        direccion = direccion;
+    }
+
+    public String getsnro_lote() {
+        return nro_lote;
+    }
+
+    public void setNro_lote(String nro_lote ) {
+        nro_lote = nro_lote;
+    }
+
+    public String getsprovincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia ) {
+        provincia = provincia;
+    }
+
+    public String getsdistrito() {
+        return distrito;
+    }
+
+    public void setDistrito(String distrito ) {
+        distrito = distrito;
+    }
+
+    public String getscelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        celular = celular;
+    }
+
+    public String getsemail() {
+        return email;
+    }
+
+    public void setEmail(String email ) {
+        email = email;
+    }
+
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña ) {
+        contraseña = contraseña;
+    }
+
+    public String getNro_tarjeta() {
+        return nro_tarjeta;
+    }
+
+    public void setNro_tarjeta(String nro_tarjeta ) {
+        nro_tarjeta = nro_tarjeta;
+    }
+
+    public String getvigencia_tarjeta() {
+        return vigencia_tarjeta;
+    }
+
+    public void setVigencia_tarjeta(String vigencia_tarjeta) {
+        vigencia_tarjeta = vigencia_tarjeta;
+    }
+
+}
