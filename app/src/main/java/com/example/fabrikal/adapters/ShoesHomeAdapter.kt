93d8f1ab0b234205620938031d8e688b9c8a2390
Ricardo Cortez <cortez.ricardo19@gmail.com>
2021-04-26 15:02:01 -0500

    ajustes

--- app/src/main/java/com/example/fabrikal/adapters/ShoesHomeAdapter.kt
+++ app/src/main/java/com/example/fabrikal/adapters/ShoesHomeAdapter.kt
@@ -1,7 +1,5 @@
 package com.example.fabrikal.adapters
 
-import android.content.Context
-//import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,7 +18,7 @@ class ShoesHomeAdapter(val lista : List<ShoeHomeItem> ) : RecyclerView.Adapter<S
         init {
             itemView.setOnClickListener {
 
-                onItemClick?.invoke(lista[adapterPosition])
+                onItemClick?.invoke(lista[absoluteAdapterPosition])
             }
         }
 
