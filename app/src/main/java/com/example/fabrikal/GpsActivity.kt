3bc412b7c14d0253c8b931df85f5803b5cc65603
Ricardo Cortez <cortez.ricardo19@gmail.com>
2021-04-26 23:01:09 -0500

    ajustes

--- app/src/main/java/com/example/fabrikal/GpsActivity.kt
+++ app/src/main/java/com/example/fabrikal/GpsActivity.kt
@@ -1,6 +1,5 @@
 package com.example.fabrikal
 
-//import android.support.v7.app.AppCompatActivity
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -8,7 +7,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -25,7 +23,7 @@ class GpsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_gps)
         createMapFragment()
     }
 
