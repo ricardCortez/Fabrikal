d09d0593ec3347557107305f87ffdf42786efd5e
Ricardo Cortez <cortez.ricardo19@gmail.com>
2021-04-27 00:17:14 -0500

    ajustes permisos

--- app/src/main/java/com/example/fabrikal/CamaraActivity.kt
+++ app/src/main/java/com/example/fabrikal/CamaraActivity.kt
@@ -1,11 +1,84 @@
 package com.example.fabrikal
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_camara.*
+import permissions.dispatcher.*
 
+@RuntimePermissions
 class CamaraActivity : AppCompatActivity() {
+
+    companion object{
+        const val  REQUEST_IMAGE_CAPTURE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camara)
+        buttonCamara.setOnClickListener {
+            abrirCamaraWithPermissionCheck()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        onRequestPermissionsResult(requestCode, grantResults)
+    }
+
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    fun abrirCamara() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(
+                takePictureIntent,
+                REQUEST_IMAGE_CAPTURE
+            )
+        }
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    fun showRationaleForCamera(request: PermissionRequest) {
+        showRationaleDialog("Queremos tomar fotos", request)
+    }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    fun onCameraDenied() {
+        Toast.makeText(this, "permiso denegado", Toast.LENGTH_SHORT).show()
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    fun onCameraNeverAskAgain() {
+        Toast.makeText(this, "nunca preguntar", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showRationaleDialog(
+        texto: String,
+        request: PermissionRequest
+    ) {
+        AlertDialog.Builder(this)
+            .setPositiveButton("Permitir") { _, _ -> request.proceed() }
+            .setNegativeButton("Denegar") { _, _ -> request.cancel() }
+            .setCancelable(false)
+            .setMessage(texto)
+            .show()
+    }
+
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
